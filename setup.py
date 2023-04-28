2079829207d938ae8dea3b0a6e50ddb8311d6cb0
Zahar A. Ponimash <ponimashz@mail.ru>
2023-04-28 16:40:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except ImportError:
     from distutils.core import setup, find_namespace_packages
  
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+REQUIREMENTS = [line.strip() for line in open("requirements.txt").readlines()]
 
 setup(
     name='RuNerLib',
     version='0.0.1',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    packages=find_namespace_packages(include=['RuNerLib']),
+    packages=find_namespace_packages(include=['RuNerLib','RuNerLib.*']),
     install_requires=REQUIREMENTS
 )
